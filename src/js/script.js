54c768432ed0ca16208c6ec1e460aa5be9ecf5a4
jochemjanssens <janssens.jochem@gmail.com>
2017-10-30 21:57:44 +0100

    basis webgl implementatie

--- src/js/script.js
+++ src/js/script.js
@@ -1,66 +1,146 @@
 let analyzer;
-const svgns = `http://www.w3.org/2000/svg`;
 const elements = 1024;
 let audio;
 
 const init = () => {
   setupEquilizer();
+  createScene();
+  createLights();
+  createEquilizer();
 };
 
 const setupEquilizer = () => {
-  audio = new Audio();
-  audio.controls = true;
-  audio.src = `../assets/audio/ultralife.mp3`;
-  audio.addEventListener(`playing`, renderFrame);
-  document.body.appendChild(audio);
-
+  audio = document.querySelector(`audio`);
   const audioCtx = new AudioContext();
   analyzer = audioCtx.createAnalyser();
 
   const sourceNode = audioCtx.createMediaElementSource(audio);
   sourceNode.connect(analyzer);
   sourceNode.connect(audioCtx.destination);
+};
 
-  createEquilizer();
+let scene, camera, fieldOfView, aspectRatio, nearPlane, farPlane, renderer, container, HEIGHT, WIDTH;
+const createScene = () => {
+  HEIGHT = window.innerHeight;
+  WIDTH = window.innerWidth;
+
+  scene = new THREE.Scene();
+
+  scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+
+  aspectRatio = WIDTH / HEIGHT;
+  fieldOfView = 60;
+  nearPlane = 1;
+  farPlane = 10000;
+  camera = new THREE.PerspectiveCamera(
+		fieldOfView,
+		aspectRatio,
+		nearPlane,
+		farPlane
+  );
+
+  camera.position.set(0, 100, 200);
+
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true
+  });
+
+  renderer.setSize(WIDTH, HEIGHT);
+  renderer.shadowMap.enabled = true;
+  container = document.querySelector(`.visual`);
+  container.appendChild(renderer.domElement);
+};
+
+let hemisphereLight, shadowLight, ambientLight;
+const createLights = () => {
+	// A hemisphere light is a gradient colored light;
+	// the first parameter is the sky color, the second parameter is the ground color,
+	// the third parameter is the intensity of the light
+  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .9);
+
+	// A directional light shines from a specific direction.
+	// It acts like the sun, that means that all the rays produced are parallel.
+  shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+
+	// Set the direction of the light
+  shadowLight.position.set(150, 350, 350);
+
+	// Allow shadow casting
+  shadowLight.castShadow = true;
+
+	// define the visible area of the projected shadow
+  shadowLight.shadow.camera.left = - 400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = - 400;
+  shadowLight.shadow.camera.near = 100;
+  shadowLight.shadow.camera.far = 1000;
+
+	// define the resolution of the shadow; the higher the better,
+	// but also the more expensive and less performant
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+
+  ambientLight = new THREE.AmbientLight(0xdc8874, .5);
+
+	// to activate the lights, just add them to the scene
+  scene.add(hemisphereLight);
+  scene.add(shadowLight);
+  scene.add(ambientLight);
+};
+
+//const minX = - 100;
+//const maxX = 100;
+//const minY = 0;
+//const maxY = 200;
+
+let xCounter = 0;
+let yCounter = 0;
+
+const bars = [];
+
+const createEquilizer = () => {
+  let bar;
+  for (let i = 0;i < elements;i ++) {
+    if (xCounter < 32) {
+      xCounter ++;
+    } else {
+      xCounter = 0;
+      yCounter ++;
+    }
+    bar = new Bar();
+    bar.mesh.position.x = - 100 + ((200 / 32) * xCounter);
+    bar.mesh.position.y = (200 / 32) * yCounter;
+    bar.mesh.position.z = - 100;
+    scene.add(bar.mesh);
+    bars.push(bar);
+  }
   renderFrame();
 };
 
-const createEquilizer = () => {
-  const $svg = document.querySelector(`.equilizer`);
-  const width = $svg.getAttribute(`width`);
-  const height = $svg.getAttribute(`height`);
+class Bar {
+  constructor() {
+    this.mesh = new THREE.Object3D();
 
-  for (let i = 0;i < elements;i ++) {
-    const rect = document.createElementNS(svgns, `rect`);
-    rect.setAttributeNS(null, `x`, (width / elements) * i);
-    rect.setAttributeNS(null, `y`, height);
-    rect.setAttributeNS(null, `height`, 0);
-    rect.setAttributeNS(null, `width`, (width / elements));
-    rect.setAttributeNS(null, `fill`, `#000`);
-    rect.classList.add(`item${i}`);
-    $svg.appendChild(rect);
+    const geomBox = new THREE.BoxGeometry(2, 2, 2);
+    const matBox = new THREE.MeshPhongMaterial({color: 0x00ff00});
+    const box = new THREE.Mesh(geomBox, matBox);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    this.mesh.add(box);
   }
-};
+}
 
 const renderFrame = () => {
   const data = new Uint8Array(analyzer.frequencyBinCount);
   analyzer.getByteFrequencyData(data);
 
-  const $svg = document.querySelector(`.equilizer`);
-  const height = $svg.getAttribute(`height`);
-
-  for (let i = 0;i < elements;i ++) {
-    const $item = $svg.querySelector(`.item${i}`);
-    const itemData = data[i];
-    $item.setAttribute(`height`, itemData);
-    $item.setAttribute(`y`, height - itemData);
+  renderer.render(scene, camera);
+  for (let i = 0;i < data.length;i ++) {
+    bars[i].mesh.position.z = - 100 + (data[i] / 2);
   }
-
-  if (!audio.paused) {
-    requestAnimationFrame(renderFrame);
-  }
+  requestAnimationFrame(renderFrame);
 };
-
-
 
 init();
